b065268370974b91750807a2b2eb60a82312bb03
abb-cf <106682621+abb-cf@users.noreply.github.com>
2022-08-15 15:44:21 -0400

    adds mongoose expressions, takes out temp topTenMovies, adds mongoose to app.get for /movies endpoint

--- index.js
+++ index.js
@@ -1,131 +1,130 @@
-const express = require("express");
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+
+const Movies = Models.Movies;
+const Users = Models.Users;
+
+mongoose.connect('mongodb://localhost:27017/cinefiledb', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const express = require("express"),
+    bodyParser = require('body-parser'),
+    uuid = require('uuid'),
     morgan = require('morgan');
 
 const app = express();
 
-let topTenMovies = [
-    {
-        title: 'Princess Diaries',
-        year: 2001,
-        director: 'Gary Marshall',
-        actors: ['Anne Hathaway', 
-        'Julie Andrews', 
-        'Héctor Elizondo',
-        'Heather Matarazzo',
-        'Mandy Moore',
-        'Caroline Goodall',
-        'Robert Schwartzman']
-    },
-    {
-        title: 'Mean Girls',
-        year: 2004,
-        director: 'Mark Waters',
-        actors: ['Lindsay Lohan',
-        'Rachel McAdams',
-        'Tim Meadows',
-        'Ana Gasteyer',
-        'Amy Poehler',
-        'Tina Fey']
-    },{
-        title: 'Pride and Prejudice',
-        year: 2005,
-        director: 'Joe Wright',
-        actors: ['Kiera Knightley',
-        'Matthew Macfadyen',
-        'Brenda Blethyn',
-        'Donald Sutherland',
-        'Tom Hollander',
-        'Rosamund Pike',
-        'Jena Malone',
-        'Judi Dench']
-    },{
-        title: 'Hustle',
-        year: 2022,
-        director: 'Jeremiah Zagar',
-        actors: ['Adam Sandler',
-        'Queen Latifah',
-        'Ben Foster',
-        'Juancho Hermangomez',
-        'Robert Duvall',
-        'Anthony Edwards',
-        'Heidi Gardner']
-    },{
-        title: 'Ingrid Goes West',
-        year: 2017,
-        director: 'Matt Spicer',
-        actors: ['Aubrey Plaza',
-        'Elizabeth Olsen',
-        'Billy Magnussen',
-        'Wyatt Russell',
-        'Pom Klementieff',
-        'O\'Shea Jackson Jr.']
-    },{
-        title: 'Everything Everywhere All At Once',
-        year: 2022,
-        director: ['Daniel Kwan', 'Daniel Scheinert'],
-        actors: ['Michelle Yeoh',
-        'Shetphanie Hsu',
-        'Ke Huy Quan',
-        'Jenny Slate',
-        'Harry Shum Jr.',
-        'James Hong',
-        'Jamie Lee Curtis']
-    },{
-        title: 'Fargo',
-        year: 1996,
-        director: 'Joel Coen',
-        actors: ['Frances McDormand',
-        'William H. Macy',
-        'Steve Buscemi',
-        'Harve Presnell',
-        'Peter Storemare']
-    },{
-        title: 'Little Women',
-        year: 2019,
-        director: 'Greta Gerwig',
-        actors: ['Saoirse Ronan',
-        'Emma Watson',
-        'Florence Pugh',
-        'Eliza Scanlen',
-        'Laura Dern',
-        'Timothee Chalamet',
-        'Meryl Streep',
-        'Tracy Letts',
-        'Bob Odenkirk',
-        'James Norton'
-        ]
-    },{
-        title: 'Lady Bird',
-        year: 2017,
-        director: 'Greta Gerwig',
-        actors: ['Saoirse Ronan',
-        'Laurie Metcalf',
-        'Tracy Letts',
-        'Timothee Chalamet',
-        'Beanie Feldstein',
-        'Stephen McKinley Henderson',
-        'Lois Smith']
-    },{
-        title: 'Runaway Bride',
-        year: 1999,
-        director: 'Gary Marshall',
-        actors: ['Julia Roberts',
-        'Richard Gere',
-        'Joan Cusack',
-        'Hector Elizondo',
-        'Rita Wilson',
-        'Paul Dooley']
-    },
-];
+app.use(bodyParser.json());
 
 app.use(morgan('common'));
 
 app.get('/', (req, res) => {
     res.send('Welcome to Cine-File!');
   });
-  
+
+//returns a list of all movies to the user
 app.get('/movies', (req, res) => {
-    res.json(topTenMovies);
+    Movies.find()
+        .then((movies) => {
+            res.status(201).json(movies);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+  });
+
+// Gets the data about a single movie, by title  
+app.get('/movies/:title', (req, res) => {
+    res.json(topTenMovies.find((movie) =>
+    { return movie.title === req.params.title }));
+});
+
+// Gets the data about a single genre 
+app.get('/movies/genre', (req, res) => {
+    res.send('Successful GET request for data on a specific genre.');
+    // res.json(movies.find((genre) =>
+    // { return movie.genre === req.params.genre }));
+});
+
+// Gets the data about a single director, by name  
+app.get('/movies/director', (req, res) => {
+    res.send('Successful GET request for data on a specific director.');
+    // res.json(movies.find((director) =>
+    // { return movie.director === req.params.director }));
+});
+  
+// Adds data for a new user
+app.post('/users', (req, res) => {
+    // let newUser = req.body;
+
+    res.send('Successful POST of new user.');
+
+    // if (!newUser.name) {
+    //     const message = 'Missing name in request body';
+    //     res.status(400).send(message);
+    // } else {
+    //     newUser.id = uuid.v4();
+    //     users.push(newUser);
+    //     res.status(201).send(newUser);
+    // }
+});
+
+ // Update the username of a user
+ app.put('/users/username', (req, res) => {
+
+    res.send('Successful PUT, changing user usernmae information.');
+
+    // let username = users.find((user) => { return user.username === req.params.username });
+    // let newUsername = req.body;
+
+    // if (user) {
+    //   user.username[req.params.username] = parseInt(req.params.username);
+    //   res.status(201).send('User ' + req.params.username + ' was updated to' + req.params.newUsername);
+    // } else {
+    //   res.status(404).send('User ' + req.params.username + ' was not found.');
+    // }
+  });
+
+// Adds movie to a user's list of favorites
+app.post('/users/favorites', (req, res) => {
+    
+    res.send('Successful POST of new favorite movie to user list of favorites.');
+    
+    // let favorite = req.body;
+
+    // if (!favorite.title) {
+    //     const message = 'Missing title in request body';
+    //     res.status(400).send(message);
+    // } else {
+    //     users.push(favorite);
+    //     res.status(201).send(favorite);
+    // }
+});
+
+// Deletes a movie from a user's list of favorites
+app.delete('users/favorites', (req, res) => {
+    
+    res.send('Successful DELETE of movie from use list of favorites.');
+    
+    // let favorite = favorites.find((title) => { return favorites/title === req.params.title });
+
+    // if (title) {
+    //     title = favorites.title((obj) => { return obj.favorite !== req.params.title});
+    //     res.status(201).send('Movie' + req.params.title + ' was removed from favorites.');
+    // }
+});
+
+// Deletes a user from the app by username
+app.delete('/users/username', (req, res) => {
+   
+    res.send('Successful DELETE of user.');
+   
+    // let username = users.find((user) => { return user.username === req.params.username });
+  
+    // if (user) {
+    //   user = users.filter((obj) => { return obj.username !== req.params.username });
+    //   res.status(201).send('User ' + req.params.username + ' was deleted.');
+    // }
   });
 
 app.use(express.static('public'));
