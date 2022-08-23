7d578e1d615ba8df54811f7002feafc2040022d8
abb-cf <106682621+abb-cf@users.noreply.github.com>
2022-08-23 10:26:45 -0400

    adds encryption to password

--- models.js
+++ models.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'),
+    bcrypt = require('bcrypt');
 
 let movieSchema = mongoose.Schema ({
     Title: {type: String, required: true},
@@ -23,6 +24,14 @@ let userSchema = mongoose.Schema({
     Birthday: Date,
     Favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movies' }]
 });
+
+userSchema.statistics.hashPassword = (password) => {
+    return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function(password) {
+    return bcrypt.compareSync(password, this.Password);
+};
 
 let Movies = mongoose.model('Movies', movieSchema);
 let Users = mongoose.model('Users', userSchema);
