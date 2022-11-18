2ebcc5eb36df1aa58c1cc0d171e9f4243ed1e49b
abb-cf <106682621+abb-cf@users.noreply.github.com>
2022-11-17 21:16:54 -0500

    CORS configuration for netlify site & create-react-app urls

--- index.js
+++ index.js
@@ -18,8 +18,21 @@ mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifie
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 const cors = require('cors');
-app.use(cors());
+let allowedOrigins = ['https://cine-file.netlify.app', 'http://localhost:3000/'];
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            let message = 'The CORS policy for this application doesnt allow access from origin ' + origin;
+            return callback(new Error(message), false);
+        }
+        return callback(null, true);
+    }
+}));
+
 let auth = require('./auth.js') (app);
 
 const passport = require('passport');
