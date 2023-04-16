a5b56fc3f7134b4b676a5e010d69e4583e3eccf8
abb-cf <106682621+abb-cf@users.noreply.github.com>
2023-04-15 22:13:44 -0400

    launching via vercel and a mongodb extension

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const Models = require('./models.js');
 const Movies = Models.Movies;
 const Users = Models.Users;
 
-mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
